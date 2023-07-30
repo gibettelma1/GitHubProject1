f8a4460650f7abbdf061445f3a038409fcd8b669
gibettelma1 <140761564+gibettelma1@users.noreply.github.com>
2023-07-29 22:41:52 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -16,5 +16,8 @@ public class DemoDay2 {
 	public static void divede(int a, int b) {
 		System.out.println(a/b);
 	}
+	//add method
+	public static void add(int a , int b,int c) {
+		System.out.println(a+b+c);
 
 }
