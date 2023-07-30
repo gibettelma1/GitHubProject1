2de3c11c35a082ad6a050b02f250f625dc8afc70
gibet <gibet@10.0.0.234>
2023-07-29 22:23:06 -0400

    implemented substract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,5 +4,13 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		System.out.println("Hi Gibet:Test");
 	}
+	//add method
+	public static void add(int a , int b) {
+		System.out.println(a+b);
+	}
+	//substract
+	public static void substract(int a, int b) {
+		System.out.println(a-b);
+	}
 
 }
